cf0597f5c79c0636db75bc84c506d4d0cf2c5b0e
Junar Alinsub <junaralinsub2@protonmail.com>
2022-04-27 10:23:00 +0800

    Add reset functionality

--- solutions/solution1.js
+++ solutions/solution1.js
@@ -1,11 +1,23 @@
 const solution1 = (() => {
   if (window.iceCreamFlavors.length && document.readyState === "complete") {
-    const ul = document.getElementsByClassName("ice-cream-list")[0];
-    window.iceCreamFlavors.map((flavor) => {
-      const li = document.createElement("li");
-      li.appendChild(document.createTextNode(flavor));
-      li.classList.add("ice-cream-flavor");
-      ul.appendChild(li);
-    });
+    // check if list is manipulated
+    if ($(document).data("initialList")) {
+      //reset if list is manipulated
+      $(document).data("initialList").replaceAll(".ice-cream-list");
+    }
+
+    // clone list for check later
+    $(document).data("initialList", $(".ice-cream-list").clone(true));
+
+    const timeOut = setTimeout(() => {
+      const ul = document.getElementsByClassName("ice-cream-list")[0];
+      window.iceCreamFlavors.map((flavor) => {
+        const li = document.createElement("li");
+        li.appendChild(document.createTextNode(flavor));
+        li.classList.add("ice-cream-flavor");
+        ul.appendChild(li);
+      });
+      clearTimeout(timeOut);
+    }, 500);
   }
 })();
--- solutions/solution2.js
+++ solutions/solution2.js
@@ -1,9 +1,21 @@
 $(document).ready(() => {
-  $("ul").append(
-    () =>
-      window.iceCreamFlavors.length &&
-      window.iceCreamFlavors.map(
-        (flavor) => `<li class="ice-cream-flavor">${flavor}</li>`
-      )
-  );
+  // check if list is manipulated
+  if ($(document).data("initialList")) {
+    //reset if list is manipulated
+    $(document).data("initialList").replaceAll(".ice-cream-list");
+  }
+
+  // clone list for check later
+  $(document).data("initialList", $(".ice-cream-list").clone(true));
+
+  const timeOut = setTimeout(() => {
+    $("ul").append(
+      () =>
+        window.iceCreamFlavors.length &&
+        window.iceCreamFlavors.map(
+          (flavor) => `<li class="ice-cream-flavor">${flavor}</li>`
+        )
+    );
+    clearTimeout(timeOut);
+  }, 500);
 });
